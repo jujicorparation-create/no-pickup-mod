c9ef2f6ebbcb583b336088706159151b4c4a4c4e
jujicorparation-create <jujicorparation@gmail.com>
2026-06-10 01:59:59 +0500

    Create NoPickupNetwork.java

---
+++ src/main/java/com/nopickup/mod/NoPickupNetwork.java
@@ -0,0 +1,8 @@
+package com.nopickup.mod;
+
+import net.minecraft.util.Identifier;
+
+public class NoPickupNetwork {
+    public static final Identifier TOGGLE_PACKET_ID =
+            Identifier.of("nopickup", "toggle_pickup");
+}
