d80583275d961bbbff646075f7def17d565ee698
jujicorparation-create <jujicorparation@gmail.com>
2026-06-10 02:24:20 +0500

    Update NoPickupNetwork.java

--- src/main/java/com/nopickup/mod/NoPickupNetwork.java
+++ src/main/java/com/nopickup/mod/NoPickupNetwork.java
@@ -1,8 +1,24 @@
 package com.nopickup.mod;
 
+import net.minecraft.network.RegistryByteBuf;
+import net.minecraft.network.codec.PacketCodec;
+import net.minecraft.network.codec.PacketCodecs;
+import net.minecraft.network.packet.CustomPayload;
 import net.minecraft.util.Identifier;
 
-public class NoPickupNetwork {
-    public static final Identifier TOGGLE_PACKET_ID =
-            Identifier.of("nopickup", "toggle_pickup");
-}
+public record NoPickupNetwork(boolean disable) implements CustomPayload {
+
+    public static final Id<NoPickupNetwork> ID =
+            new Id<>(Identifier.of("nopickup", "toggle_pickup"));
+
+    public static final PacketCodec<RegistryByteBuf, NoPickupNetwork> CODEC =
+            PacketCodec.tuple(
+                    PacketCodecs.BOOL, NoPickupNetwork::disable,
+                    NoPickupNetwork::new
+            );
+
+    @Override
+    public Id<? extends CustomPayload> getId() {
+        return ID;
+    }
+        }
